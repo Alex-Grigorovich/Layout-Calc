4a6ebd469d55d93578507e800ec0bc32be48b226
Alex-Grigorovich <holdencolfield37@gmail.com>
2020-02-20 02:09:41 +0300

    add function calculate for Site

--- script/script.js
+++ script/script.js
@@ -1,3 +1,18 @@
+const DATA ={
+    whitchSite:['landing', 'multiPage','onlineStore'],
+    price:[4000,8000,26000],
+    desktopTemplates:[50,40,30],
+    adapt:20,
+    mobileTempates:15,
+    editable:10,
+    metrikaYandex:[500,1000,2000],
+    analyticsGoogle:[850,1350,3000],
+    sendOrder:500,
+    deadlineDay:[[2,7],[3,10],[7,14]],
+    deadlinePercent:[20,17,15],
+}
+
+
 const startButton = document.querySelector('.start-button');
 firstScreen = document.querySelector('.first-screen');
 mainForm = document.querySelector('.main-form');
@@ -5,20 +20,87 @@ formCalculate = document.querySelector('.form-calculate');
 endButton = document.querySelector('.end-button');
 totalElem = document.querySelector('.total');
 fastRange = document.querySelector('.fast-range');
+totalPriceSum = document.querySelector('.total_price__sum');
+adapt = document.querySelector('#adapt');
 
 
 function showElem(elem){
     elem.style.display = 'block';
 };
 
+// function showAdapt(){
+//     if(adapt.getAttribute('checked')){
+//         document.getElementById('mobileTemplates').setAttribute("disabled", "true");
+//     }
+// }
+// showAdapt()
+
 
 function hideElem(elem){
     elem.style.display = 'none';
 };
 
+function priceCalculation(elem){
+let result = 0,
+index = 0,
+options =[];
+
+
+    if(elem.name === 'whichSite'){
+        for(const item of formCalculate.elements){
+           if(item.type === 'checkbox'){
+               item.checked = false;
+           }
+    }
+    hideElem(fastRange);
+    }
+
+
+    for(const item of formCalculate.elements){
+        if(item.name === 'whichSite' && item.checked){
+            index = DATA.whitchSite.indexOf(item.value);
+        }else if(item.classList.contains('calc-handler') && item.checked){
+           options.push(item.value)
+            
+        }
+    }
+
+options.forEach(function(key){
+    if(typeof(DATA[key])==='number'){
+        if(key==='sendOrder'){
+            result += DATA[key]
+        }else{
+            result += DATA.price[index] * DATA[key] / 100
+        }
+    }else{
+        if(key ==='desktopTemplates'){
+            result += DATA.price[index] * DATA.desktopTemplates[index] / 100
+        }else{
+            result += DATA[key][index];
+        }
+    }
+})
+
+result += DATA.price[index];
+
+
+    totalPriceSum.textContent = result;
+}
+
 function handlerCallbackForm(event){
     const target = event.target;
-       target.checked ? showElem(fastRange) : hideElem(fastRange); 
+
+    if(target.classList.contains('want-faster')){
+        target.checked ? showElem(fastRange) : hideElem(fastRange); 
+    }
+       
+
+    if(target.classList.contains('calc-handler')){
+        priceCalculation(target);
+    }
+
+
+
 }
 
 startButton.addEventListener('click', function(){
@@ -36,7 +118,7 @@ endButton.addEventListener('click', function(){
     
 showElem(totalElem);
 
-})
+});
 
 
 formCalculate.addEventListener('change', handlerCallbackForm);
